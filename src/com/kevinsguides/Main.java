3edcbe7e1cb6e72af40cc36776ffc03e192e1266
kevin <kevin@kevinsguides.com>
2021-12-25 10:25:38 -0500

    First Update

---
+++ src/com/kevinsguides/Main.java
@@ -0,0 +1,35 @@
+package com.kevinsguides;
+
+/**
+ * The Main Class
+ * Used to start Games of Blackjack
+ * @author Kevin Olson - KevinsGuides.com
+ * @License MIT
+ * @Copyright Copyright 2021 Kevin Olson
+ * @Version 2.01 - Seems stable but not extensively tested - updated with more docs
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        //print out a welcome message
+        System.out.println("Welcome to BlackJack.");
+
+        //start a new  game of Blackjack
+        Game blackjack = new Game();
+
+
+
+    }
+}
